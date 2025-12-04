c146e89d253b388896afdf99ab39aff2ef3d4710
Muhamad Allya Agung <mallyaagung@gmail.com>
2025-12-04 12:55:58 +0700

    chore

--- routes/company.routes.js
+++ routes/company.routes.js
@@ -7,40 +7,40 @@ const path = require("path");
 const { companies } = require("../models");
 
 router.get("/options", auth, CompanyController.getOptions);
-// router.get("/import", async (req, res) => {
-//   try {
-//     const filePath = "./file/Data IUP.xlsx";
+router.get("/import", async (req, res) => {
+  try {
+    const filePath = "./file/Data IUP.xlsx";
 
-//     // Read Excel file
-//     const workbook = XLSX.readFile(filePath);
-//     const sheet = workbook.Sheets[workbook.SheetNames[0]];
-//     const rows = XLSX.utils.sheet_to_json(sheet);
+    // Read Excel file
+    const workbook = XLSX.readFile(filePath);
+    const sheet = workbook.Sheets[workbook.SheetNames[0]];
+    const rows = XLSX.utils.sheet_to_json(sheet);
 
-//     if (!rows.length) {
-//       return res.status(400).json({ message: "Excel file is empty" });
-//     }
+    if (!rows.length) {
+      return res.status(400).json({ message: "Excel file is empty" });
+    }
 
-//     // Reformat rows for DB
-//     const dataToInsert = rows.map((row) => ({
-//       name: row.Perusahaan || null,
-//     }));
+    // Reformat rows for DB
+    const dataToInsert = rows.map((row) => ({
+      name: row.Perusahaan || null,
+    }));
 
-//     // Filter out empty names
-//     const finalData = dataToInsert.filter((r) => r.name);
+    // Filter out empty names
+    const finalData = dataToInsert.filter((r) => r.name);
 
-//     // Bulk Insert
-//     await companies.bulkCreate(finalData, {
-//       ignoreDuplicates: true, // avoids errors if duplicate names exist
-//     });
+    // Bulk Insert
+    await companies.bulkCreate(finalData, {
+      ignoreDuplicates: true, // avoids errors if duplicate names exist
+    });
 
-//     return res.json({
-//       message: "Companies imported successfully",
-//       totalImported: finalData.length,
-//     });
-//   } catch (err) {
-//     console.error(err);
-//     return res.status(500).json({ error: err.message });
-//   }
-// });
+    return res.json({
+      message: "Companies imported successfully",
+      totalImported: finalData.length,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: err.message });
+  }
+});
 
 module.exports = router;
